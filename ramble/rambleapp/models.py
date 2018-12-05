8f863fb443f48e4d23411fa893f76fd240164766
priyaven <priyaven@gmail.com>
2018-12-05 00:20:42 -0800

    Added tables for users, posts, likes and shares.

--- ramble/rambleapp/models.py
+++ ramble/rambleapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+  user_name = models.CharField(max_length=16)
+
+class Post(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  post_timestamp = models.DateTimeField('date published')
+  post_text = models.CharField(max_length=10000)
+
+class Like(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class Share(models.Model):
+  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+

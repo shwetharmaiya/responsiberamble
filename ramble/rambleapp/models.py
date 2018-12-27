c5d394d67e5cd6e7c5845b85855599c8f3c733fe
Priya Venkateshan <prvenkateshan@ebay.com>
2018-12-27 00:23:39 -0800

    All tables reference the django user table. No need for post form.

--- ramble/rambleapp/models.py
+++ ramble/rambleapp/models.py
@@ -1,24 +1,28 @@
 from django.db import models
 
+from django.contrib.auth.models import User as Auth_User
+
+from datetime import datetime
+
 # Create your models here.
 
-class User(models.Model):
-  user_name = models.CharField(max_length=16)
-
 class Post(models.Model):
-  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-  post_timestamp = models.DateTimeField('date published')
+  user_id = models.ForeignKey(Auth_User, on_delete=models.CASCADE)
+  post_timestamp = models.DateTimeField(default=datetime.now)
   post_text = models.CharField(max_length=10000)
 
+
 class Like(models.Model):
-  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  user_id = models.ForeignKey(Auth_User, on_delete=models.CASCADE)
   post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+
 
 class Share(models.Model):
-  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  user_id = models.ForeignKey(Auth_User, on_delete=models.CASCADE)
   post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
 
+
 class Comment(models.Model):
-  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  user_id = models.ForeignKey(Auth_User, on_delete=models.CASCADE)
   post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
   comment_text = models.CharField(max_length=1000)
